b7f3f799777215cfcdbe7c6491a387e88b733225
Daniel Springer <dcs@Daniels-MacBook-Pro.local>
2017-11-01 15:49:13 -0700

    added cell for Realtor

---
+++ RealtorList/Cells/RealtorCell.swift
@@ -0,0 +1,19 @@
+//
+//  RealtorCell.swift
+//  RealtorList
+//
+//  Created by Daniel Springer on 11/1/17.
+//  Copyright © 2017 Daniel Springer. All rights reserved.
+//
+
+import UIKit
+
+class RealtorCell: UITableViewCell {
+    
+}
+
+extension RealtorCell {
+    func configureWithRealtor(_ realtor: Realtor) {
+        
+    }
+}
