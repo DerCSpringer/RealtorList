be9311b1467046c810925fb81f55b9c859d0da90
Daniel Springer <dcs@Daniels-MacBook-Pro.local>
2017-11-01 12:47:12 -0700

    added detailsVM/VC

---
+++ RealtorList/ViewControllers/DetailsVM.swift
@@ -0,0 +1,13 @@
+//
+//  DetailsVM.swift
+//  RealtorList
+//
+//  Created by Daniel Springer on 11/1/17.
+//  Copyright © 2017 Daniel Springer. All rights reserved.
+//
+
+import Foundation
+
+class DetailsVM {
+    
+}
