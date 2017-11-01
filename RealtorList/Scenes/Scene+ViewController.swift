70f0f7b8a6aaf3052ad647dbd95bb32ef7407398
Daniel Springer <dcs@Daniels-MacBook-Pro.local>
2017-11-01 13:08:15 -0700

    implemented Scene+ViewController and named Storyboard ID for Navigation controllers

--- RealtorList/Scenes/Scene+ViewController.swift
+++ RealtorList/Scenes/Scene+ViewController.swift
@@ -9,5 +9,21 @@
 import UIKit
 
 extension Scene {
-
+    func viewController() -> UIViewController {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        switch self { // Switch on the type of scene we want to display
+        case .detailsForRealtor(let viewModel):
+            let nc = storyboard.instantiateViewController(withIdentifier: "DetailsVC") as! UINavigationController
+            var vc = nc.viewControllers.first as! DetailsVC
+            // We call this bind after the view is loaded and outlets are loaded
+            vc.bindViewModel(to: viewModel)
+            return nc
+            
+        case .realtorList(let viewModel):
+            let nc = storyboard.instantiateViewController(withIdentifier: "RealtorListVC") as! UINavigationController
+            var vc = nc.viewControllers.first as! RealtorListVC
+            vc.bindViewModel(to: viewModel)
+            return nc
+        }
+    }
 }
