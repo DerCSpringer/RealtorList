9f0ad475910b4f1e3458a2e07ff5370e5181953f
Daniel Springer <dcs@Daniels-MacBook-Pro.local>
2017-11-01 13:04:29 -0700

    implemented SceneCoordinatorType

--- RealtorList/Scenes/SceneCoordinatorType.swift
+++ RealtorList/Scenes/SceneCoordinatorType.swift
@@ -9,5 +9,13 @@
 import UIKit
 
 protocol SceneCoordinatorType {
+    init(window: UIWindow)
+    func transition(to scene: Scene, type: SceneTransitionType)
+    func pop(animated: Bool) -> Void
+}
 
+extension SceneCoordinatorType {
+    func pop() -> Void {
+        return pop(animated: true)
+    }
 }
