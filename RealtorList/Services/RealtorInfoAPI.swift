8a65bcb9025806b09d772733a2e796eae890fead
Daniel Springer <dcs@Daniels-MacBook-Pro.local>
2017-11-01 13:17:06 -0700

    added Realtor API class

---
+++ RealtorList/Services/RealtorInfoAPI.swift
@@ -0,0 +1,13 @@
+//
+//  RealtorInfoAPI.swift
+//  RealtorList
+//
+//  Created by Daniel Springer on 11/1/17.
+//  Copyright © 2017 Daniel Springer. All rights reserved.
+//
+
+import Foundation
+
+class RealtorInfoAPI {
+    
+}
