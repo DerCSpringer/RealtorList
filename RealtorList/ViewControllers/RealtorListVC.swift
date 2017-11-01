e7ab76f9942f7425af5a14b7c423af2d1cd9f8b4
Daniel Springer <dcs@Daniels-MacBook-Pro.local>
2017-11-01 12:56:38 -0700

    added RealtorListVC/VM

---
+++ RealtorList/ViewControllers/RealtorListVC.swift
@@ -0,0 +1,13 @@
+//
+//  RealtorListVC.swift
+//  RealtorList
+//
+//  Created by Daniel Springer on 11/1/17.
+//  Copyright © 2017 Daniel Springer. All rights reserved.
+//
+
+import UIKit
+
+class RealtorListVC: UIViewController {
+    
+}
